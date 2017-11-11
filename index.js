4de3e4a9b4e36d84c80491b2945d66eacf05e670
desmaisons_david <desmaisons_david@hotmail.com>
2017-11-10 22:20:24 -0200

    clean-up

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const toPromise = function toPromise(command, argument) {
 	if (arguments.length <= 1) {
-		return function toPromise(argument) {
+		return function(argument) {
 			return new Promise(function (fullfill, reject) {
 				var res = { fullfill: function (res) { fullfill(res); }, reject: function (err) { reject(new Error(err)); } };
 				command.Execute(argument, res);
